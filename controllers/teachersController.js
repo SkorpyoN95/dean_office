dccb9fd01cceaf209f672e5fcf9191e8701adcac
SkorpyoN95 <dominik.chojny@gmail.com>
2018-06-18 18:02:35 +0200

    Listing teachers

--- controllers/teachersController.js
+++ controllers/teachersController.js
@@ -1,7 +1,12 @@
 var teacher = require('../models/teacher');
+var mock = require('../models/mock-user');
 
 exports.teacher_list = function(req, res){
-    res.send('NOT IMPLEMENTED: Teachers list');
+    teacher.find({}, 'first_name last_name')
+    .exec(function(err, docs){
+        if(err) return next(err);
+        res.render('listing', {title: 'Teachers', user: mock.user, group: 'teachers', collection: docs});
+    });
 };
 
 exports.teacher_details = function(req, res){
